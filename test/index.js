85e6b8e358ec7fe209fc12616fd9de3ba208bf6a
Dzmitry Shaplyka <dzmitry_shaplyka@epam.com>
2023-02-20 13:49:18 +0300

    initial commit

---
+++ test/index.js
@@ -0,0 +1,43 @@
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const { jsonParser, sendRequest } = require('../src');
+
+describe('Fetch method', () => {
+  let responseJson;
+
+  before(async () => {
+    await sendRequest();
+  });
+
+  beforeEach(() => {
+    responseJson = require('../src/response.json');
+  });
+
+  it('should receive items', () => {
+    responseJson.forEach(item => expect(typeof item).to.be.equal('object'));
+  });
+
+  it('should contain ids below 20', () => {
+    responseJson.map(({ id }) => id).forEach(id => expect(id).to.be.below(20));
+  });
+});
+
+describe('JSON Parser', () => {
+  let parserJson;
+
+  before(async () => {
+    await jsonParser();
+  });
+
+  beforeEach(() => {
+    parserJson = require('../src/parsed.json');
+  });
+
+  it('.html should be replaced for every item', () => {
+    expect(parserJson.every(({ docId }) => docId.includes('html'))).to.be.equal(false);
+  });
+
+  it('every item should contain http://doc.epam.com/ string', () => {
+    expect(parserJson.every(({ docId }) => docId.includes('http://doc.epam.com/'))).to.be.equal(true);
+  });
+});
